09e11d79a0864e5e7696f8fc64f6b586eb7c01b0
James Pasko <jbpasko@gmail.com>
2013-01-17 16:22:34 -0800

    More work

--- portfolios/models.py
+++ portfolios/models.py
@@ -1,16 +1,37 @@
 from django.db import models
 from django.contrib.auth.models import User
+from imagekit.models import ImageSpecField
+from imagekit.models.fields import ProcessedImageField
+from imagekit.processors import ResizeToFill, ResizeToFit
+
+def upload_to_photo(instance, filename):
+    """
+    Creates an upload_to path for photos.
+    """
+    return 'images/%s/%s' % (instance.gallery.user.id, filename)
+
+def upload_to_profile_photo(instance, filename):
+    """
+    Same, but for profile photos.
+    """
+    return 'images/%s/%s' % (instance.user.id, filename)
 
 class Gallery(models.Model):
     user = models.ForeignKey(User)
     title = models.CharField(max_length=75, blank=True)
+    description = models.TextField(blank=True)
 
     def __unicode__(self):
         return u'User: %s, Title: %s' % (self.user.username, self.title)
 
 class Photo(models.Model):
     gallery = models.ForeignKey(Gallery)
-    image = models.ImageField(upload_to='images/')
+    image = ProcessedImageField([ResizeToFit(width=640,
+                                             height=640,
+                                             upscale=False)],
+                                upload_to=upload_to_photo)
+    thumbnail = ImageSpecField([ResizeToFill(50, 50)],
+                               image_field='image')
     caption = models.CharField(max_length=140, blank=True)
 
     def __unicode__(self):
@@ -18,7 +39,7 @@ class Photo(models.Model):
 
 class ProfilePhoto(models.Model):
     user = models.OneToOneField(User)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to=upload_to_profile_photo)
 
     def __unicode__(self):
         return u'Profile image for %s' % self.user.username
