728332cc929b1975ebee81af6f6e17e9b49a0304
James Pasko <jbpasko@gmail.com>
2013-01-20 17:58:22 -0800

    Fixed some template issues

---
+++ static/js/confirm-delete.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    $("#dialog").dialog({
+      modal: true,
+            bgiframe: true,
+            width: 500,
+            height: 200,
+      autoOpen: false
+      });
+
+    $("a.confirm").click(function(e) {
+        e.preventDefault();
+        var theHREF = $(this).attr("href");
+
+        $("#dialog").dialog('option', 'buttons', {
+                "Confirm" : function() {
+                    window.location.href = theHREF;
+                    },
+                "Cancel" : function() {
+                    $(this).dialog("close");
+                    }
+                });
+
+        $("#dialog").dialog("open");
+
+    });
+
+});
