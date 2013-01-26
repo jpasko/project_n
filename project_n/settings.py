5567343f65dacd9a66f6c3b652ff6c46ea29d683
James Pasko <jbpasko@gmail.com>
2013-01-26 11:25:16 -0800

    changed sqlite db name

--- project_n/settings.py
+++ project_n/settings.py
@@ -17,7 +17,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_ROOT, 'sqlit3.db'),
+        'NAME': os.path.join(PROJECT_ROOT, 'sqlite3.db'),
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
