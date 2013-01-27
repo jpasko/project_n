07f1da735cea5be106031c4926d4103432e54cda
James Pasko <jbpasko@gmail.com>
2013-01-26 18:49:51 -0800

    still getting S3 to work properly

--- project_n/settings.py
+++ project_n/settings.py
@@ -1,7 +1,7 @@
 # Used by Heroku to configure the database.
 import dj_database_url
 import os.path
-PROJECT_ROOT = os.path.dirname(__file__)
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -47,28 +47,20 @@ USE_TZ = True
 
 # AWS settings.
 AWS_STORAGE_BUCKET_NAME = 'pasko'
-AWS_ACCESS_KEY_ID = 'AKIAJQXBCMVWJLI566OQ'
-AWS_SECRET_ACCESS_KEY = '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'
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, '/../media')
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+AWS_PRELOAD_METADATA = True
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'poo'
+MEDIA_URL = 'http://s3.amazonaws.com/%s/media/' % AWS_STORAGE_BUCKET_NAME
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, '/../static')
+ADMIN_MEDIA_PREFIX = 'http://s3.amazonaws.com/%s/static/admin/' % AWS_STORAGE_BUCKET_NAME
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'http://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = 'http://s3.amazonaws.com/%s/static/' % AWS_STORAGE_BUCKET_NAME
 
 # Additional locations of static files
 STATICFILES_DIRS = (
