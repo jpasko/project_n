6904189d13240cfcb3d08f3638247128d9eea16b
James Pasko <jbpasko@gmail.com>
2013-02-22 13:10:08 -0800

    STEP 1 (cont): A few tweaks

--- project_n/user_urls.py
+++ project_n/user_urls.py
@@ -8,6 +8,55 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    # The user's main profile, which shows their galleries.
+    url(r'^$', 'portfolios.views.portfolio'),
+
+    # A gallery.
+    url(r'^gallery/(\d+)/$', 'portfolios.views.gallery'),
+
+    # The about page of a user's profile.
+    url(r'^about/$', 'portfolios.views.about'),
+
+    # Users can edit their profile.
+    url(r'^edit/$', 'portfolios.views.edit'),
+
+    # Users must create a gallery to upload photos into.
+    url(r'^create_gallery/$', 'portfolios.views.create_gallery'),
+
+    # Users can upload new photos using the upload form.
+    url(r'^upload/$', 'portfolios.views.upload'),
+
+    # Users can upload photos directly to a gallery as well.
+    url(r'^upload/(\d+)/$', 'portfolios.views.upload'),
+
+    # Deletes the photo, and redirects back to the gallery.  If
+    # the gallery is empty, redirects back to the main profile.
+    url(r'^photo/(\d+)/delete/$', 'portfolios.views.delete_photo'),
+
+    # Edit a photo's caption.
+    url(r'^photo/(\d+)/edit/$', 'portfolios.views.edit_photo'),
+
+    # Deletes a gallery, and redirect back to the main portfolio.
+    url(r'^gallery/(\d+)/delete/$', 'portfolios.views.delete_gallery'),
+
+    # Edit a gallery's title and thumbnail.
+    url(r'^gallery/(\d+)/edit/$', 'portfolios.views.edit_gallery'),
+
+    # Deletes the profile photo, and redirects back to the profile (about page).
+    url(r'^about/delete_photo/$', 'portfolios.views.delete_profile_photo'),
+
+    # Allows the user to change their account settings.
+    url(r'^settings/$', 'accounts.views.change_settings'),
+
+    # Change the credit card form.
+    url(r'^accounts/change/$', 'accounts.views.change_credit_card'),
+
+    # Account upgrade/downgrade page.  The view will handle free vs. paid users.
+    url(r'^accounts/(\w+)/$', 'accounts.views.change_account'),
+
+    # Add a credit card form.
+    url(r'^accounts/(\w+)/payment/$', 'accounts.views.add_credit_card'),
+
     # Logs out the user, redirects to /logout/success/
     url(r'^logout/$', 'accounts.views.logout_user'),
 
@@ -20,69 +69,12 @@ urlpatterns = patterns('',
     # Reorder the photos within a gallery.
     url(r'^reorder_photos/$', 'portfolios.views.change_photo_order'),
 
-    # Password reset URLs.
-    url(r'^accounts/password/reset/$', 'django.contrib.auth.views.password_reset',
-        {'post_reset_redirect' : '/accounts/password/reset/done/'}),
-    url(r'^accounts/password/reset/done/$', 'django.contrib.auth.views.password_reset_done'),
-    url(r'^accounts/password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', 
-        {'post_reset_redirect' : '/accounts/password/done/'}),
-    url(r'^accounts/password/done/$', 'django.contrib.auth.views.password_reset_complete'),
-
     # Deletes the user's account.
     url(r'^delete/$', 'accounts.views.delete_user'),
 
     # Robots.txt directly from template.
     url(r'^robots\.txt$', direct_to_template,
         {'template': 'robots.txt', 'mimetype': 'text/plain'}),
-
-    # The user's main profile, which shows their galleries.
-    url(r'^(\w+)/$', 'portfolios.views.portfolio'),
-
-    # A gallery.
-    url(r'^(\w+)/gallery/(\d+)/$', 'portfolios.views.gallery'),
-
-    # The about page of a user's profile.
-    url(r'^(\w+)/about/$', 'portfolios.views.about'),
-
-    # Users can edit their profile.
-    url(r'^(\w+)/edit/$', 'portfolios.views.edit'),
-
-    # Users must create a gallery to upload photos into.
-    url(r'^(\w+)/create_gallery/$', 'portfolios.views.create_gallery'),
-
-    # Users can upload new photos using the upload form.
-    url(r'^(\w+)/upload/$', 'portfolios.views.upload'),
-
-    # Users can upload photos directly to a gallery as well.
-    url(r'^(\w+)/upload/(\d+)/$', 'portfolios.views.upload'),
-
-    # Deletes the photo, and redirects back to the gallery.  If
-    # the gallery is empty, redirects back to the main profile.
-    url(r'^(\w+)/photo/(\d+)/delete/$', 'portfolios.views.delete_photo'),
-
-    # Edit a photo's caption.
-    url(r'^(\w+)/photo/(\d+)/edit/$', 'portfolios.views.edit_photo'),
-
-    # Deletes a gallery, and redirect back to the main portfolio.
-    url(r'^(\w+)/gallery/(\d+)/delete/$', 'portfolios.views.delete_gallery'),
-
-    # Edit a gallery's title and thumbnail.
-    url(r'^(\w+)/gallery/(\d+)/edit/$', 'portfolios.views.edit_gallery'),
-
-    # Deletes the profile photo, and redirects back to the profile (about page).
-    url(r'^(\w+)/about/delete_photo/$', 'portfolios.views.delete_profile_photo'),
-
-    # Allows the user to change their account settings.
-    url(r'^(\w+)/settings/$', 'accounts.views.change_settings'),
-
-    # Change the credit card form.
-    url(r'^(\w+)/accounts/change/$', 'accounts.views.change_credit_card'),
-
-    # Account upgrade/downgrade page.  The view will handle free vs. paid users.
-    url(r'^(\w+)/accounts/(\w+)/$', 'accounts.views.change_account'),
-
-    # Add a credit card form.
-    url(r'^(\w+)/accounts/(\w+)/payment/$', 'accounts.views.add_credit_card'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
