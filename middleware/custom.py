e88ee93b4a7fc708bf2e9f84cd50e4e2718e8d48
James Pasko <jbpasko@gmail.com>
2013-03-20 14:28:46 -0700

    experiment

--- middleware/custom.py
+++ middleware/custom.py
@@ -19,3 +19,24 @@ class SubdomainURLs:
         else:
             request.subdomain = None
             request.urlconf = settings.MAIN_URLS
+
+class ParseURLs:
+    """
+    """
+
+    def process_request(self, request):
+        scheme = "http" if not request.is_secure() else "https"
+        path = request.get_full_path()
+        folio24 = request.META.get('SERVER_NAME')
+        pieces = folio24.split('.')
+        subdomain = ".".join(pieces[:-2])
+        domain = request.META.get('HTTP_HOST')
+        if domain == 'www.jamespasko.com':
+            request.subdomain = 'jpasko'
+            request.urlconf = settings.USER_URLS
+        elif subdomain != 'www' and subdomain != '' and subdomain is not None:
+            request.subdomain = subdomain
+            request.urlconf = settings.USER_URLS
+        else:
+            request.subdomain = None
+            request.urlconf = settings.MAIN_URLS
--- project_n/settings.py
+++ project_n/settings.py
@@ -104,7 +104,8 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'middleware.custom.SubdomainURLs',
+    # 'middleware.custom.SubdomainURLs',
+    'middleware.custom.ParseURLs',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
