cc433200b51b20ef091f24c645452183a9e20f80
James Pasko <jbpasko@gmail.com>
2013-03-22 19:00:48 -0700

    minor fix

--- middleware/custom.py
+++ middleware/custom.py
@@ -54,5 +54,5 @@ class RedirectToCustomDomain:
     """
 
     def process_request(self, request):
-        if request.domain:
+        if request.domain and not request.user.is_authenticated():
             return HttpResponseRedirect('http://www.' + request.domain + request.path)
