d78e2fb63554cf5b3d54f4fdd4d4ab56d5ff7a58
James Pasko <jbpasko@gmail.com>
2013-03-22 19:03:38 -0700

    database error

--- accounts/models.py
+++ accounts/models.py
@@ -129,7 +129,7 @@ class UserProfile(models.Model):
 # Model associating domain names with users
 class Domains(models.Model):
     user = models.OneToOneField(User)
-    username = models.CharField()
+    username = models.CharField(max_length=30)
     domain = models.CharField(max_length=255)
     pending = models.BooleanField(default=True)
     # not used at the moment
