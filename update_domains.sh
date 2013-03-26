5b86097bed388f6d2c640b3f67d9b77598068bd0
James Pasko <jbpasko@gmail.com>
2013-03-25 17:05:39 -0700

    update_domains runs from wherever

--- update_domains.sh
+++ update_domains.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-heroku run python manage.py get_new_domains | grep ^* | xargs heroku domains:add
+heroku run python manage.py get_new_domains --app shrouded-mountain-1305 | grep ^* | xargs heroku domains:add --app shrouded-mountain-1305
